cbe05eaafd978b08a4dceca04b4ce34bea732e6d
AngelFHC <angel@attune.co>
2024-01-31 00:05:15 -0600

    Added coding challenge

---
+++ README.md
@@ -0,0 +1,65 @@
+
+# Coding Challenge
+
+## Description
+
+To test your familiarity or ability to pick up our stack, we will ask you to perform 2 tasks:
+- Modify a CRUD API that uses Node, Typescript, and the Firestore Emulator
+- Create a tiny react project that uses the previously mentioned API
+
+When you finish both tasks (or as much as you are able to), please do the following:
+- Upload this repository (with your changes) into your own private repository on GitHub
+- Invite the GitHub users `AngelFHC` and `CrisFHC` to your repository
+
+## Task 1
+
+The entire project will run locally on your machine using Firebase's emulators for their cloud services. The API uses Express and sits behind a Firebase function. All the API routes are already stubbed out. Your task is to fill them out. All the specifications are in the comments in [functions/src/index.ts](functions/src/index.ts).
+
+Read the comments closely: they contain a few tips that will clarify certain peculiarities of Firestore.
+
+- Implement the stubbed out APIs in [functions/src/index.ts](functions/src/index.ts), there are only 3 and these are marked with `TODO`
+
+- If something does not make sense or is not very clear in the specification, use your best judgement to infer what was needed
+
+
+### Task 1 Pre-requisites (if you already have these on your machine, you may not need to do these steps)
+- Install [Node.js](https://nodejs.org/en)
+    - To verify that you have node installed, open your terminal and run `node -v`
+- Install [OpenJDK](https://jdk.java.net/21/) (needed for the local Firestore emulator)
+    - These [instructions](https://www.freecodecamp.org/news/install-openjdk-free-java-multi-os-guide/) may help
+    - To verify that you have JDK installed, open your terminal and run `java -version`
+
+### Task 1 Setup
+- Clone this repository to your machine
+
+- Install Firebase Tools globally `npm install -g firebase-tools`
+
+- In your terminal, navigate to the respository and to the `functions` directory to run: `npm run install`
+
+- To start the API and the database emulator, sumply run `npm run conc`
+    - To verify that the API started properly, in your browser go to the URL that is defined by:
+        - `[Firebase] ✔  functions[]: http function initialized (...)` in the terminal
+        - It should look something like `http://127.0.0.1:5001/challenge-project/us-central1/api`
+        - Once you open this URL in your browser, you should see `alive 💪` as a response
+    - Do the same for the database, this one sits inside a table:
+        - `View in Emulator UI` > `Firestore`
+        - It should look something like `http://127.0.0.1:4000/firestore`
+        - Once you open this URL in your browser, you should see a UI for the firestore database with a few pre-populated collections
+
+### Task 1 Notes
+- All the code is in the `functions` folder, and you only need to modify the files in the `/src/index.ts` file
+- Every single time you rerun `npm run conc`, your data will be erased on the Firestore Emulator and replaced with a base set of data, be careful!
+- If you are ever confused at what objects your APIs should create, look at the prepopulated objects in the Firestore through the `Emulator UI`
+
+
+## Task 2
+
+This is more of an open-ended task: using the most recent version of React, you should create a tiny webapp where you can see a table that displays the `quizzes` information from Task 1.
+
+We only have a few requirements:
+- Make use of the API you modified in Task 1
+- Make sure that the table's data is updated automatically
+- Make sure it's mobile friendly
+- Add the project in the root folder, and name it `client`
+
+Note: Your webapp does NOT need to be able to create/delete quizzes, it only needs to display the data 
---
+++ functions/src/index.ts
@@ -0,0 +1,236 @@
+import * as functions from 'firebase-functions';
+
+import * as bodyParser from "body-parser";
+import * as express from "express"
+
+import * as admin from "firebase-admin"
+
+import { asyncHandler, buildValidator } from "./middleware"
+import Joi = require('@hapi/joi');
+import { DocumentReference } from 'firebase-admin/firestore';
+
+
+// 
+// If you are not familiar with Firebase/firestore, that's ok! Here is some helpful documentation:
+// (Make sure to scroll past all the setup instructions!)
+// https://firebase.google.com/docs/firestore/query-data/get-data
+//
+
+admin.initializeApp()
+const Firestore = admin.firestore()
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", asyncHandler(async (request, response, next) => {
+  response.send("alive 💪")
+}))
+
+/*
+ *
+ * TIP:
+ * 
+ * for field validation use Joi
+ * there is a middleware `buildValidator` already defined
+ * just figure out how to define the right Joi schema and use the middleware
+ * 
+ * the 'buildValidator' middleware will also handle all the errors
+ */
+
+/*
+ *
+ * TIP:
+ * 
+ * database models
+ * 
+ * USER:
+ *   collection name: 'users'
+ *   object structure:
+ *     { 
+ *       name: string, 
+ *       quizIds: string[] (defaults to [])
+ *     }
+ * 
+ * QUIZ:
+ *   collection name: 'quizzes'
+ *   object structure:
+ *     { 
+ *       name: string, 
+ *       description: string, (optional)
+ *       active: boolean,
+ *       userCount: number (defaults to 0)
+ *     }
+*/
+
+/*
+ * EXAMPLE: create a user
+ *
+ * 
+ * ROUTE: 
+ *   POST /users
+ *
+ * Expected request body JSON:
+ * {
+ *   name: "Bob Builder"
+ * }
+ * 
+ * Validation info:
+ *   name: string (required, minimum length 1)
+ *  
+ * 
+ * EXPECTED RESPONSE:
+ * JSON
+ * {
+ *   "user": {
+ *     "id": "uAWoWFpknToBcdZ7GF59",
+ *     "quizIds": [],
+ *     "name": "Bob Builder"
+ *   }
+ * }
+ * 
+ */
+
+// These are the firestore collections where users and quizzes are stored
+const usersColl = Firestore.collection("users")
+const quizzesColl = Firestore.collection("quizzes")
+
+//
+// This is a sample Joi validation schema, for more info see its documentation
+// https://joi.dev/api/?v=17.12.0
+//
+const userSchema = Joi.object({
+  name: Joi.string()
+    .min(1)
+    .required()
+})
+
+// 
+// This is an example of how to use the buildValidator, as well as the firestore packages
+// Try it out! Do a POST request to /users
+//
+app.post("/users",
+  buildValidator(userSchema, "body"),
+  asyncHandler(async (request, response, next) => {
+    const userRef: DocumentReference = await usersColl.add({
+      name: request.body.name,
+      quizIds: []
+    })
+
+    const userDoc = await userRef.get()
+
+    response.json({
+      user: {
+        id: userDoc.id,
+        ...userDoc.data()
+      }
+    })
+  }))
+
+/*
+ * get a user
+ *
+ * 
+ * ROUTE: 
+ *   GET /users/:userId
+ *  
+ * EXPECTED RESPONSE:
+ * JSON
+ * {
+ *   "user": {
+ *     "id": "uAWoWFpknToBcdZ7GF59",
+ *     "quizIds": [],
+ *     "name": "Bob Builder"
+ *   }
+ * }
+ * 
+ */
+app.get("/users/:userId",
+  asyncHandler(async (request, response, next) => {
+
+    // @TODO: IMPLEMENT ME
+
+    response.json("IMPLEMENT ME")
+  })
+)
+
+
+/*
+ * create an individual quiz
+ *
+ * Expected request body JSON:
+ * {
+ *   name: "Quiz 2",
+ *   description: "this is a quiz to do something",
+ *   active: false
+ * }
+ * 
+ * Validation info:
+ *   name: string (required, can't be blank)
+ *   description: string (optional, can't be blank if submitted)
+ *   active: boolean (optional, defaults to false)
+ * 
+ * ROUTE: 
+ *   POST /quizzes
+ * 
+ * EXPECTED RESPONSE:
+ * JSON
+ * {
+ *   "quiz": {
+ *     "id": "KltYLDxCbP5lX6BHWY9l",
+ *     "description": "this is a quiz to do something",
+ *     "active": false,
+ *     "userCount": 0,
+ *     "name": "Quiz 2",
+ *     "createdOn": {
+ *       "_seconds": 1595465567,
+ *       "_nanoseconds": 643000000
+ *     }
+ *   }
+ * }
+ * 
+ * 
+ * TIP: for your createdOn value, you have 2 options:
+ * Use firestore's documentSnapshot's createTime
+ * or
+ * Use a JS date object
+ * 
+ */
+app.post("/quizzes",
+  asyncHandler(async (request, response, next) => {
+
+    // @TODO: IMPLEMENT ME
+
+    response.json("IMPLEMENT ME")
+  })
+)
+
+
+
+/*
+ * list latest 10 quizzes (ignore pagination)
+ *
+ * ROUTE: 
+ *   GET /quizes
+ * 
+ * EXPECTED RESPONSE:
+ * JSON
+ * {
+ *   quizes: [
+ *      ... dump all the properties of the quiz ...,
+ *      ... dump all the properties of the quiz ...,
+ *      ...
+ *   ]
+ * }
+ * 
+ */
+app.get("/quizzes",
+  asyncHandler(async (request, response, next) => {
+    
+    // @TODO: IMPLEMENT ME
+
+    response.json("IMPLEMENT ME")
+  })
+)
+
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(app);
